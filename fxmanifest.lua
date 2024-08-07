ee56f78d164bb958d99920e89bbc7a9515efe8d5
Wizdom <125174856+ShawnBrownmean@users.noreply.github.com>
2024-08-07 16:06:59 -0400

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,41 @@
+fx_version 'cerulean'
+use_experimental_fxv2_oal 'yes'
+game "gta5"
+lua54 'yes'
+
+author "Byte Labs"
+version '1.0.0'
+description 'Customize your virtual persona with precision using the Byte Labs Appearance Menu'
+repository 'https://github.com/Byte-Labs-Studio/bl_appearance'
+
+ui_page 'build/index.html'
+--ui_page 'http://localhost:3001/' --for dev
+
+server_scripts {
+    'data/config.lua',
+    'dist/server/**/*.js'
+}
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'dist/shared/**/*.js',
+}
+
+client_scripts {
+    'data/*',
+    'dist/client/**/*.js'}
+
+files {
+    'build/**',
+}
+
+file_set 'locale' {
+    'locale/*'
+}
+
+dependencies {
+    'bl_bridge',
+    'bl_sprites'
+}
+
+provide 'illenium-appearance'
